b384adb91708c4b4de8e405ec7d571c73c2a7691
chokoladis <you@example.com>
2022-09-29 10:56:53 +0400

    start media

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+// // gsap
+// var gsap = new TimelineMax();
+// const contoller = new ScrollMagic.Controller();
+
+// gsap.to("#Features", .5,{ 
+//   // scrollTrigger: {
+//   //   trigger: '#Features',
+//   //   toggleActions: "restart pause reverse pause"
+//   // },
+//   scale: 1,
+//   cycle: {
+//     y: [-50, 50]
+//   },
+//   ease: Elastic.easeOut,
+//   stagger: {
+//     from: "center",
+//     amount: 0.25
+//   },
+  
+//   opacity: 1,
+//   duration: 1
+// });
